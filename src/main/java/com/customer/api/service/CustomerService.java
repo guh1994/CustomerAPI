75e779f36b149d3f8910ca979c09a51177409b29
alex_santos <alex@santocodigo.com.br>
2024-04-09 21:41:48 -0300

    [API] - Refactory RestEntityResponse

--- src/main/java/com/customer/api/service/CustomerService.java
+++ src/main/java/com/customer/api/service/CustomerService.java
@@ -1,111 +1,117 @@
 package com.customer.api.service;
 
-import com.customer.api.model.PersistentCustomer;
-import com.customer.api.repository.CustomerRepository;
-import com.customer.api.rest.RestCustomer;
-import com.customer.api.validator.RestEntityResponse;
-import org.apache.logging.log4j.util.Strings;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.customer.api.model.PersistentCustomer;
+import com.customer.api.repository.CustomerRepository;
+import com.customer.api.rest.RestCustomer;
+import com.customer.api.validator.RestEntityResponse;
+
 @Service
-public class CustomerService {
+public class CustomerService
+{
 
     @Autowired
     private CustomerRepository repository;
 
-    public List<RestCustomer> getCustomers() {
+    public List<RestCustomer> getCustomers()
+    {
 
         final List<PersistentCustomer> allCustomers = repository.findAll();
 
-        return RestCustomer.convert(allCustomers);
+        return RestCustomer.convert( allCustomers );
     }
 
     public RestCustomer getCustomerById(
-            final Integer id) {
+        final Integer id )
+    {
 
-        final Optional<PersistentCustomer> persistentCustomer = repository.findById(id);
-        if (persistentCustomer.isPresent()) {
-            return RestCustomer.convert(persistentCustomer.get());
+        final Optional<PersistentCustomer> persistentCustomer = repository.findById( id );
+        if( persistentCustomer.isPresent() ) {
+            return RestCustomer.convert( persistentCustomer.get() );
         }
 
         return null;
     }
 
     public RestEntityResponse<RestCustomer> createCustomer(
-            final RestCustomer customer) {
+        final RestCustomer customer )
+    {
 
-        final List<String> messages = validateCommons(customer);
-        if (!messages.isEmpty()) {
-            return RestEntityResponse.createError(messages);
+        final List<String> messages = validateCommons( customer );
+        if( ! messages.isEmpty() ) {
+            return RestEntityResponse.createError( messages );
         }
 
-        // TODO Validate in database existsByField
-
-        boolean existsByEmail = repository.existsByEmail(customer.email());
-        if (existsByEmail) {
-            messages.add("Customer allready exist with this email");
-            return RestEntityResponse.createError(messages);
+        final boolean existsByEmail = repository.existsByEmail( customer.email() );
+        if( existsByEmail ) {
+            messages.add( "Customer allready exist with this email" );
+            return RestEntityResponse.createError( messages );
         }
 
-        final PersistentCustomer persistedCustomer = repository.save(PersistentCustomer.create(customer));
-        return RestEntityResponse.createSuccess(RestCustomer.convert(persistedCustomer));
+        final PersistentCustomer persistedCustomer = repository.save( PersistentCustomer.create( customer ) );
+        return RestEntityResponse.createSuccess( RestCustomer.convert( persistedCustomer ) );
 
     }
 
     private List<String> validateCommons(
-            final RestCustomer customer) {
-        if (customer == null) {
-            return List.of("O Payload está nulo.");
+        final RestCustomer customer )
+    {
+        if( customer == null ) {
+            return List.of( "O Payload está nulo." );
         }
         final List<String> messages = new ArrayList<>();
-        if (Strings.isEmpty(customer.email())) {
-            messages.add("O email está inválido");
+        if( Strings.isEmpty( customer.email() ) ) {
+            messages.add( "O email está inválido" );
         }
-        if (Strings.isEmpty(customer.name())) {
-            messages.add("O nome está inválido");
+        if( Strings.isEmpty( customer.name() ) ) {
+            messages.add( "O nome está inválido" );
         }
         return messages;
     }
 
     public RestEntityResponse<RestCustomer> updateCustomer(
-            final RestCustomer customer,
-            final Integer id) {
+        final RestCustomer customer,
+        final Integer id )
+    {
 
-        final List<String> messages = validateCommons(customer);
-        if (!messages.isEmpty()) {
-            return RestEntityResponse.createError(messages);
+        final List<String> messages = validateCommons( customer );
+        if( ! messages.isEmpty() ) {
+            return RestEntityResponse.createError( messages );
         }
 
-        final Optional<PersistentCustomer> persistedCustomer = repository.findById(id);
-        if (!persistedCustomer.isPresent()) {
-            messages.add("Customer with id %s not exists".formatted(id));
-            return RestEntityResponse.createError(messages);
+        final Optional<PersistentCustomer> persistedCustomer = repository.findById( id );
+        if( ! persistedCustomer.isPresent() ) {
+            messages.add( "Customer with id %s not exists".formatted( id ) );
+            return RestEntityResponse.createError( messages );
         }
 
         final PersistentCustomer updatedCustomer = persistedCustomer.get();
-        updatedCustomer.update(customer);
+        updatedCustomer.update( customer );
 
-        return RestEntityResponse.createSuccess(RestCustomer.convert(updatedCustomer));
+        return RestEntityResponse.createSuccess( RestCustomer.convert( updatedCustomer ) );
 
     }
 
     public RestEntityResponse<RestCustomer> deleteCustomer(
-            final Integer id) {
-        if (id == null) {
-            List<String> messages = new ArrayList<>();
-            messages.add("Customer deletion id is null.");
+        final Integer id )
+    {
+        if( id == null ) {
+            final List<String> messages = new ArrayList<>();
+            messages.add( "Customer deletion id is null." );
 
-            return RestEntityResponse.deleteError(messages);
+            return RestEntityResponse.createError( messages );
         }
 
-        repository.deleteById(id);
+        repository.deleteById( id );
 
-        return RestEntityResponse.deleteSuccess();
+        return RestEntityResponse.createSuccess( "Customer Deleted with success" );
     }
 
 }
---
+++ src/test/java/com/customer/api/service/CustomerServiceTest.java
@@ -0,0 +1,10 @@
+package com.customer.api.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith( MockitoExtension.class )
+class CustomerServiceTest
+{
+
+}
